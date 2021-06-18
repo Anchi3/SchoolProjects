e90b67d6163792454c2fafc3d4549a94d4809369
AnzeloMadarang <anzelo@gmail.com>
2021-06-17 20:34:34 -0500

    Added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./components/navbar";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import axios from "axios";
+import Doctors from "./components/doctors";
+import Patient from "./components/patient";
+
+class App extends Component {
+  state = {
+    doctors: [],
+    patient: {},
+    currentDoctorId: 0,
+    currentSymptom: "",
+  };
+
+  componentDidMount() {
+    axios.get("https://localhost:5001/Doctors").then((response) => {
+      this.setState({ doctors: response.data });
+      this.setState({ currentDoctorId: this.state.doctors[0].id });
+    });
+  }
+
+  handleCurrentDoctor = (event) => {
+    console.log(event.target.value);
+    this.setState({ currentDoctorId: event.target.value });
+  };
+
+  handleCurrentPatient = (event) => {
+    console.log(event);
+    this.setState({ patient: event });
+  };
+
+  handleSymptom = (e) => {
+
+    this.setState({ currentSymptom: e.target.value });
+
+
+  };
+
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar>Doctor's Patient Intake Form</NavBar>
+        <main className="container">
+          <br />
+          <p>
+            Current Doctor ID: &nbsp;
+            {this.state.currentDoctorId}
+            <br />
+            Current Patient ID: &nbsp;
+            {this.state.patient.id}
+            <br />
+           {/*Current Patient Symptom: &nbsp;
+            {this.state.currentSymptom}*/}
+          </p>
+          <br />
+
+          <hr />
+          <b>Please Choose Your Preferred Physician:</b><br /><br />
+          <Doctors
+            doctors={this.state.doctors}
+            onCurrentDoctor={this.handleCurrentDoctor}
+          ></Doctors>
+          <br /><br />
+          <hr />
+          <Patient onCurrentPatient={this.handleCurrentPatient}></Patient>
+          <hr />
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">
+              <b>Symptom/s:</b>
+            </label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              onChange={this.handleSymptom}
+              placeholder="Please limit to 500 characters"
+            ></textarea>
+          </div>
+
+          <button
+          type="button"
+          className="btn btn-success  m-2 btn-lg"
+          disabled={this.state.isCreate}
+          onClick={this.AddNewPatient}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Create New Appointment
+        </button>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/patient.jsx
@@ -0,0 +1,151 @@
+import React, { Component } from "react";
+
+import axios from "axios";
+
+class Patient extends Component {
+  state = {
+    patient: {},
+    isCreate: false,
+  };
+
+  handleId = (id) => {
+    axios.get("https://localhost:5001/Patients/" + id).then((response) => {
+      this.setState({ patient: response.data === "" ? {} : response.data });
+      this.props.onCurrentPatient(response.data);
+      this.setState({ isCreate: response.data.name !== undefined });
+    });
+  };
+
+  UpdatePatient = () => {
+    let patient = Object.assign({}, this.state.patient);
+
+    axios
+      .put("https://localhost:5001/Patients/" + this.state.patient.id, patient)
+      .then((response) => {
+        console.log("PUT : ", response);
+      });
+  };
+
+  AddNewPatient = () => {
+    let patient = Object.assign({}, this.state.patient);
+
+    axios.post("https://localhost:5001/Patients", patient).then((response) => {
+      this.props.onCurrentPatient(patient);
+      console.log("POST : ", response);
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <b>Patient Search Id:</b>
+        <input
+          className="form-control m-2"
+          type="number"
+          placeholder="Search by Id"
+          onBlur={(e) => this.handleId(e.target.value)}
+        ></input>
+        <br />
+        <b>Patient Details:</b>
+        <br /><br />
+
+        Health Number:
+        <input
+          className="form-control m-2"
+          type="number"
+          placeholder="Please Enter the 5-digit Health Number"
+          value={this.state.patient.healthNumber || ""}
+          onChange={(e) =>
+            this.setState((prevState) => {
+              let patient = Object.assign({}, prevState.patient);
+              patient.healthNumber = e.target.value;
+              return { patient };
+            })
+          }
+        ></input>
+
+        Name:
+        <input
+          className="form-control m-2"
+          type="text"
+          placeholder="Please Enter the Patient's Name"
+          value={this.state.patient.name || ""}
+          onChange={(e) =>
+            this.setState((prevState) => {
+              let patient = Object.assign({}, prevState.patient);
+              patient.name = e.target.value;
+              return { patient };
+            })
+          }
+        ></input>
+
+        Date of Birth: 
+        <input
+          className="form-control m-2"
+          type="date"
+          onfocus="(this.type='date')"
+          onblur="(this.type='text')"
+          placeholder="Please Enter Date of Birth YYYY-MM-DD"
+          value={this.state.patient.dateOfBirth || ""}
+          onChange={(e) =>
+            this.setState((prevState) => {
+              let patient = Object.assign({}, prevState.patient);
+              patient.dateOfBirth = e.target.value;
+              return { patient };
+            })
+          }
+        ></input>
+
+        Phone:
+        <input
+          className="form-control m-2"
+          type="tel"
+          placeholder="(XXX) XXXXXXX"
+          value={this.state.patient.phoneNumber || ""}
+          onChange={(e) =>
+            this.setState((prevState) => {
+              let patient = Object.assign({}, prevState.patient);
+              patient.phoneNumber = e.target.value;
+              return { patient };
+            })
+          }
+        ></input>
+
+        Address:
+        <input
+          className="form-control m-2"
+          type="text"
+          placeholder="Please Enter the Current Address"
+          value={this.state.patient.address || ""}
+          onChange={(e) =>
+            this.setState((prevState) => {
+              let patient = Object.assign({}, prevState.patient);
+              patient.address = e.target.value;
+              return { patient };
+            })
+          }
+        ></input>
+        <br />
+
+        <button
+          type="button"
+          className="btn btn-outline-success  m-2 btn-sm"
+          disabled={this.state.isCreate}
+          onClick={this.AddNewPatient}
+        >
+          Add New Patient
+        </button>
+        <button
+          type="button"
+          class="btn btn-outline-warning m-2 btn-sm"
+          disabled={!this.state.isCreate}
+          onClick={this.UpdatePatient}
+        >
+          Update Patient
+        </button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Patient;
